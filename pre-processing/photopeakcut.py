5d759eb2956c679dbc67ad4eb138737930f5ce1f
Firas Abouzahr <84940176+FirasAbouzahr@users.noreply.github.com>
2023-09-29 00:36:23 -0500

    Add files via upload

---
+++ pre-processing/photopeakcut.py
@@ -0,0 +1,88 @@
+# Firas Abouzahr 09-29-23
+
+########################################################################
+########################################################################
+'''
+This script imposes a photopeak cut
+
+'''
+#######################################################################
+#######################################################################
+
+import sys
+sys.path.append('/Users/feef/DOI-ML/Headers')
+
+
+from DOI_header import *
+from analysis_header import *
+from train_and_test import *
+
+# no need to do anything but read these files since by this stage the data has already been formatted in train_and_test.py
+trainingData = pd.read_csv(trainingFile)
+testingData = pd.read_csv(testingFile)
+
+# parse through all detector channels and fit to their photopeaks using getEnergySpectrum()
+# then use the fit parameters and specified sigma to create a reference dataframe of photopeak cuts per channel per DOI
+def getphotopeakcuts(df,sigma = 2,energy_bins = (100,0,40),save_to_file = (False,"photopeaksheet_{}um.csv".format(roughness))):
+    photopeakDict = {'ChannelID':[],'energyCut':[],'DOI':[]}
+    
+    for chanL in tqdm(np.unique(df.ChannelIDL)):
+        for depth in DOIs:
+            tempdf = df[df.DOI == depth]
+            p,_ = getEnergySpectrum(tempdf,chanL,'left',bins=energy_bins)
+            photopeakDict['ChannelID'] += [chanL]
+            photopeakDict['energyCut'] += [[p[1]-sigma*p[2],p[1]+sigma*p[2]]]
+            photopeakDict['DOI'] += [depth]
+            
+    for chanR in tqdm(np.unique(df.ChannelIDR)):
+        for depth in DOIs:
+            tempdf = df[df.DOI == depth]
+            p,_ = getEnergySpectrum(tempdf,chanR,'right',bins=energy_bins)
+            photopeakDict['ChannelID'] += [chanR]
+            photopeakDict['energyCut'] += [[p[1]-sigma*p[2],p[1]+sigma*p[2]]]
+            photopeakDict['DOI'] += [depth]
+
+    photopeakDf = pd.DataFrame(photopeakDict)
+    
+    if save_to_file[0] == True:
+        photopeakDf.to_csv(save_to_file[1],index = False)
+    
+    return photopeakDf
+
+# using a photopeakcut datasheet, we drop data do not fall within some specified number of standard deviations aways from the photopeak center
+def energycut(df,photopeakDf,filename):
+    
+    dfarray = df.to_numpy()
+    energyCutDf = pd.DataFrame(columns = df.columns)
+    index = 0
+
+    for rownum in tqdm(range(len(dfarray))):
+        row = dfarray[rownum]
+        
+        # get the energy cut for each channel at the given DOI
+        ch1_cut = photopeakDf[(photopeakDf.ChannelID == row[2]) & (photopeakDf.DOI == row[6])].energyCut.iloc[0]
+        ch1_lower_limit,ch1_upper_limit = ch1_cut[0],ch1_cut[1]
+        ch2_cut = photopeakDf[(photopeakDf.ChannelID == row[5]) & (photopeakDf.DOI == row[6])].energyCut.iloc[0]
+        ch2_lower_limit,ch2_upper_limit = ch2_cut[0],ch2_cut[1]
+        
+        energy1,energy2 = row[1],row[4]
+        
+        # only keep LORs whose energy values fall within the specified standard deviation of the photopeak mean
+        if energy1>=ch1_lower_limit and energy1 <=ch1_upper_limit and energy2>=ch2_lower_limit and energy2 <=ch2_upper_limit:
+                energyCutDf.loc[index] = row
+                index += 1
+        
+    energyCutDf.to_csv(filename,index = False)
+    
+    return 0
+
+
+# lets generate our photopeak dataframe with 2 sigma photopeak cuts
+# we only need to do this for the trainingData since the testingData should use the same channel pair(s)
+photopeakdata = getphotopeakcuts(trainingData)
+    
+    
+# here we call energycut() to impose energy cuts on both the training and testing data
+
+trainingData_with_energycut = energycut(trainingData,photopeakdata,'trainingdata_{}um_channelpair{}-{}_energycut.csv'.format(roughness,channelpair[0],channelpair[1]))
+testingData_with_energycut = energycut(testingData,photopeakdata,'testingdata_{}um_channelpair{}-{}_energycut.csv'.format(roughness,channelpair[0],channelpair[1]))
---
+++ pre-processing/train_and_test.py
@@ -0,0 +1,64 @@
+# Firas Abouzahr 09-27-23
+
+########################################################################
+########################################################################
+'''
+
+Here we read in our raw data and process it into training and testing data using functions
+from our Header script. We create new features for our data from raw data calculations such as weighted energy.
+
+'''
+#######################################################################
+#######################################################################
+
+import sys
+sys.path.append('/Users/feef/DOI-ML/Headers')
+
+
+from DOI_header import *
+from analysis_header import *
+
+
+# get our raw training and testing datasets
+
+dir = '/Users/feef/DOI_Data/' # set the directory where the data can be found
+roughness = 28 # set what roughness data we want to look at
+channelpair = (79,72) # set the channel pair to select data from
+number_to_train_with = 10000 # set number of datapoints from EACH DOI to train the algorithim with, so 50000 yields a size of 7*50000 datapoints to train with
+number_to_test_with = 2000 # set number of datapoints from EACH DOI to test the algorithim with
+
+
+# lists to append each DOI dataframe to
+trainingDataList = []
+testingDataList = []
+
+
+# We took data at 7 different DOIs, here we parse through the data for each DOI
+for depth in DOIs:
+    data = getDOIDataFrame(dir+'{}um_DOI_{}mm_coinc.txt'.format(roughness,depth),DOI=depth)
+    data = data[(data.ChannelIDL == channelpair[0]) & (data.ChannelIDR == channelpair[1])]
+    training,testing = train_and_test(data,number_to_train_with,number_to_test_with)
+
+    trainingDataList.append(training)
+    testingDataList.append(testing)
+    
+
+# concatenate our list of dataframes per DOI into one large dataframe & reset the index (for both testing and training data)
+trainingData = pd.concat(trainingDataList,ignore_index=True)
+testingData = pd.concat(testingDataList,ignore_index=True)
+
+
+# calculating additional features that we calculate directly from the observables
+# the power of NCD, normalized count differences, is discussed in the report and why we know to add this quantity is useful will be appear clear after training
+trainingData["NCD"] = getNCD(trainingData.ChargeL,trainingData.ChargeR)
+testingData["NCD"] = getNCD(testingData.ChargeL,testingData.ChargeR)
+trainingData["delta_t"] = trainingData.TimeL - trainingData.TimeR
+testingData["delta_t"] = testingData.TimeL - testingData.TimeR
+
+
+# well need to read these files in again in other scripts so might as well make these into variables
+trainingFile = 'trainingdata_{}um_channelpair{}-{}.csv'.format(roughness,channelpair[0],channelpair[1])
+testingFile = 'testingdata_{}um_channelpair{}-{}.csv'.format(roughness,channelpair[0],channelpair[1])
+
+trainingData.to_csv(trainingFile,index=False)
+testingData.to_csv(testingFile,index=False)
