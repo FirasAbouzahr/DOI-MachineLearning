2956dda18cfa9d64736d25400fcbb8870168b173
Firas Abouzahr <firasabouzahr@utexas.edu>
2023-11-08 00:51:20 -0600

    More Analysis Updates

---
+++ Analysis/DOIres.py
@@ -0,0 +1,67 @@
+# Firas Abouzahr 10-03-23
+
+########################################################################
+########################################################################
+'''
+Calculating resolutions from our NCD spectra plots!
+'''
+#######################################################################
+#######################################################################
+
+from MasterAnalysis import *
+example_calculation = True
+
+
+LUT = pd.read_csv("NCD_LUT_{}um.csv".format(roughness))
+LUT = LUT[LUT.IDL == channelpair[0]]
+print(LUT)
+NCDinfo = LUT.to_numpy()[0][2:]
+
+
+# some clever list comprehension (:
+NCDpeaks = np.array([NCDinfo[evens] for evens in range(len(NCDinfo)) if evens%2 == 0])
+NCDfwhms = np.array([NCDinfo[odds] for odds in range(len(NCDinfo)) if odds%2 != 0])
+
+fig,ax = plt.subplots()
+ax.scatter(NCDpeaks,DOIs)
+ax.errorbar(NCDpeaks,DOIs,xerr = NCDfwhms/2,fmt = "none")
+p,c = curve_fit(linear,NCDpeaks,DOIs)
+x = np.linspace(min(NCDpeaks)-.01,max(NCDpeaks)+.01,100)
+ax.plot(x,linear(x,*p),linestyle = "dashed",label = "Linear Fit")
+lims = list(ax.get_xlim())
+
+
+if example_calculation == True:
+    exampleMean = LUT["{}mm".format(at_DOI)].iloc[0]
+    exampleFWHM = LUT["{}mm_FWHM".format(at_DOI)].iloc[0]
+    
+    xmin = exampleMean - exampleFWHM/2
+    xmax = exampleMean + exampleFWHM/2
+    
+    ymin = linear(xmin,p[0],p[1])
+    ymax = linear(xmax,p[0],p[1])
+    
+    res = ymax - ymin
+    
+    ax.vlines(xmin,at_DOI,ymin,color = 'red')
+    ax.vlines(xmax,at_DOI,ymax,color = 'red')
+    ax.hlines(ymin,lims[0],xmin,color = 'red')
+    ax.hlines(ymax,lims[0],xmax,color = 'red',label = 'Resolution Estimation')
+    ax.text(xmax+abs(xmax/10),at_DOI,"Resolution at {} mm is {} mm".format(at_DOI,np.round(res,2)))
+
+
+ax.set_xlim(lims[0],lims[1])
+plt.legend()
+plt.show()
+
+
+
+
+#def getResolution(fit_params,):
+#/Users/feef/.Trash/Archive-Feb-11/DOI-paper-2023-shell-Feb-8-2023.pdf/Users/feef/.Trash/Archive-Feb-11/DOI-paper-2023-shell-Feb-8-2023.pdf
+#    xmin = y - x/2
+#    xmax = y + x/2
+#
+#    ymin = line(xmin,p[0],p[1])
+#    ymax = line(xmax,p[0],p[1])
+#
--- Analysis/MasterAnalysis.py
+++ Analysis/MasterAnalysis.py
@@ -18,10 +18,20 @@ from analysis_header import *
 
 dir = '/Users/feef/DOI_Data/'
 
+# general variables used throughout analysis directory
 roughness = 28
-channelpair = roughChannels[3] # choose a channel to examine, the scripts will also allow for looks at all DOI channels at once.
+channelpair = roughChannels[1] # choose a channel to examine, the scripts will also allow for looks at all DOI channels at once.
 energy_bins = (80,0,35) # (number of bins, lower limit, upper limit) this follows the syntax for np.histogram bins
 NCD_bins = (100,-0.37,.41) 
-at_DOI = 5 # this is specifically for energy-response plots, we look at each energy spectra on a DOI by DOI basis
+at_DOI = 20 # this is specifically for energy-response plots, we look at each energy spectra on a DOI by DOI basis
 
-
+# specific for getting NCD and calculating DOI resolutions
+NCD_LUT = 'NCD_LUT_{}um.csv'.format(roughness) # filename for NCD.py to generate all datapoints and FWHM error bars
+columns = ["IDL","IDR",
+        "2mm","2mm_FWHM",
+        "5mm","5mm_FWHM",
+        "10mm","10mm_FWHM",
+        "15mm","15mm_FWHM",
+        "20mm","20mm_FWHM",
+        "25mm","25mm_FWHM",
+        "28mm","28mm_FWHM"]
